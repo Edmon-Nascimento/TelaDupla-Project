a1c2bbe1f7217fd95b12dee0deafce78b8da9c78
EDMON NASCIMENTO SANTOS <edmoncode7@gmail.com>
2026-01-28 14:31:06 -0300

    Update index.jsx

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 function Footer(){
     return(
         <footer>
-            <p>Criado por <Link to="https://edmon-nascimento.github.io/Linktree-Edmoncode/" target="_blank">Edmoncode</Link> | &copy; Todos os direitos reservados</p>
+            <p>Criado por <Link to="https://linktree-edmoncode.vercel.app/" target="_blank">Edmoncode</Link> | &copy; Todos os direitos reservados</p>
         </footer>
     )
 }
